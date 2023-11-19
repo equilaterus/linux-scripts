5d13a193e9e55c82c12057df64f3ce11efa709b8
Daniel Cañizares Corrales <dacanizares@outlook.com>
2023-11-18 23:05:17 -0500

    Fixed github desktop

--- fedora/environment-config.sh
+++ fedora/environment-config.sh
@@ -24,8 +24,8 @@ sudo dnf install brave-browser
 sudo dnf install git-all
 
 # Github Desktop
-sudo rpm --import https://mirror.mwt.me/ghd/gpgkey
-sudo sh -c 'echo -e "[shiftkey]\nname=GitHub Desktop\nbaseurl=https://mirror.mwt.me/ghd/rpm\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://mirror.mwt.me/ghd/gpgkey" > /etc/yum.repos.d/shiftkey-desktop.repo'
+sudo rpm --import https://rpm.packages.shiftkey.dev/gpg.key
+sudo sh -c 'echo -e "[shiftkey-packages]\nname=GitHub Desktop\nbaseurl=https://rpm.packages.shiftkey.dev/rpm/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://rpm.packages.shiftkey.dev/gpg.key" > /etc/yum.repos.d/shiftkey-packages.repo'
 sudo dnf install github-desktop
 
 # Docker
